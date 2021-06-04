c3b78498ac030331f12c2d6fdf5c48e58749cb54
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2021-06-03 19:20:46 -0700

    Add files via upload

---
+++ screens/transactionScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class TransactionScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Transaction Screen
+                </Text>
+            </View>
+        )
+    }
+}
